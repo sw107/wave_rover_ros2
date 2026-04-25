94f2e5a52094c726b878818348774ec290eda9dc
sw107 <107_sw@naver.com>
2026-04-25 04:45:22 +0000

    feat: setup.py에 odom_publisher 노드 등록

--- src/wave_rover_bridge/setup.py
+++ src/wave_rover_bridge/setup.py
@@ -24,6 +24,7 @@ setup(
     entry_points={
         'console_scripts': [
             'wave_rover_bridge_node = wave_rover_bridge.wave_rover_bridge_node:main',
+            'odom_publisher = wave_rover_bridge.odom_publisher:main'
         ],
     },
 )
